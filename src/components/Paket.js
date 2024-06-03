8175f092ef1ab1aa3dcd70923199c5313274e01b
GunturAhmad <127398336+GunturAhmad@users.noreply.github.com>
2024-06-03 19:30:37 +0700

    Paket.js

--- src/components/Paket.js
+++ src/components/Paket.js
@@ -8,7 +8,7 @@ function Paket() {
         {[...Array(12)].map((_, index) => (
           <div key={index} className="col-lg-3 col-md-4 col-sm-6 mb-4">
             <div className="card paket-card">
-              <img src="/img/bali.jpg" className="card-img-top" alt="Paket Tour Bali" />
+              <img src="./img/bali.jpg" className="card-img-top" alt="Paket Tour Bali" />
               <div className="card-body">
                 <h5 className="card-title">Paket Tour Bali</h5>
                 <p className="card-text">Enjoy a trip with us at a very affordable cost and be able to enjoy the beauty of the Island of the Gods, Bali.</p>
